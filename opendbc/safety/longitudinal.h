edef2874c447330b697393eb395e5feb35610f55
mmoo758 <yccao94@gmail.com>
2026-08-17 19:01:19 +0800

    Revert "Longitudinal: Active With Gas"
    This reverts commit ef373e8fc549ffc90fb5d931e8430fd7869868f2.

--- opendbc/safety/longitudinal.h
+++ opendbc/safety/longitudinal.h
@@ -1,7 +1,7 @@
 #include "opendbc/safety/declarations.h"
 
 bool get_longitudinal_allowed(void) {
-  return controls_allowed;
+  return controls_allowed && !gas_pressed_prev;
 }
 
 // Safety checks for longitudinal actuation
